5002af9f3e2a5d80b4f0cfcddeca7745a094215b
Guillermo Valdez <g2valdez@ucsd.edu>
2016-10-27 08:22:28 -0700

    added link skeleton

--- index.js
+++ index.js
@@ -13,6 +13,34 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/login', function(request, response) {
+  response.render('pages/login');
+});
+
+app.get('/signup', function(request, response) {
+  response.render('pages/signup');
+});
+
+app.get('/home', function(request, response) {
+  response.render('pages/home');
+});
+
+app.get('/stats', function(request, response) {
+  response.render('pages/stats');
+});
+
+app.get('/mission_browser', function(request, response) {
+  response.render('pages/mission_browser');
+});
+
+app.get('/history', function(request, response) {
+  response.render('pages/history');
+});
+
+app.get('/mission', function(request, response) {
+  response.render('pages/mission');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
