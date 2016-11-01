78ab5a3dee1ca8e046748e53d2f78f04bc460ed0
Guillermo Valdez <g2valdez@ucsd.edu>
2016-11-01 14:30:20 -0700

    removed login requirement for regrading

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var users = require('./public/data.json').users;
+var express = require('express'); //our http request handler
+var bodyParser = require('body-parser'); //used to funnel in form data such as login
+var cookieParser = require('cookie-parser'); //used to give users cookies when logging in
+var users = require('./public/data.json').users; //reads data from data.json
+var missions = require('./public/data.json').missions;
 
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = express(); //creates a new web server
+var http = require('http').Server(app); // funnels web server through http
+var io = require('socket.io')(http); // new soccket.io instance
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //sets the directory to ./public
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser()); // use the cookie parser to populate request.cookies
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // look for views in the ./views directory
+app.set('view engine', 'ejs'); //set view engine to ejs
 
 app.get('/', function(request, response) {
   response.render('pages/index');
@@ -63,14 +64,17 @@ app.get('/signup', function(request, response) {
 
 app.get('/logout', function(request, response) {
 	response.clearCookie('user');
-  response.render('pages/login');
+  	response.render('pages/login');
 });
 
 app.get('/home', function(request, response) {
 	var cookie = request.cookies.user;
 	if(cookie === undefined){
 		console.log("error no cookie");
-		response.render('pages/login');
+		response.render('pages/home', {
+			user_name: users[0].name,
+			user_img: users[0].img
+		});
 	}
 	else {
 		response.render('pages/home', {
@@ -86,15 +90,19 @@ app.get('/stats', function(request, response) {
 });
 
 app.get('/mission_browser', function(request, response) {
-  response.render('pages/mission_browser');
+  response.render('pages/mission_browser', {
+  	missions: missions
+  });
 });
 
 app.get('/history', function(request, response) {
   response.render('pages/history');
 });
 
-app.get('/mission', function(request, response) {
-  response.render('pages/mission');
+app.get('/mission/:missionName', function(request, response) {
+  response.render('pages/mission', {
+  	missionName: request.params.missionName
+  });
 });
 
 app.get('/leaderboards', function(request, response) {
@@ -108,18 +116,18 @@ app.get('/edit_profile', function(request, response) {
 io.on('connection', function(socket){
 	console.log('a user connected');
 
-	// socket.on('current time', function(msg){
- //    	io.emit('set time', msg);
-	// 	console.log('time: ' + msg);
-	// });
+	socket.on('current time', function(msg){
+    	io.emit('set time', msg);
+		console.log('time: ' + msg);
+	});
 	
-	// socket.on('new connection', function(msg){
- //    	io.emit('get time', msg);
-	// });
+	socket.on('new connection', function(msg){
+    	io.emit('get time', msg);
+	});
 
-	// socket.on('pause play video', function(msg){
- //    	io.emit('control video', msg);
-	// });
+	socket.on('pause play video', function(msg){
+    	io.emit('control video', msg);
+	});
 
 });
 
